594f64fdf159669873ff55cf412be2b6e6ca04bd
Mayra Jaimes <mtj26@georgetown.edu>
2018-02-22 15:00:56 -0600

    when player hits 5 it is game over

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,18 +1,29 @@
 var p1button = document.getElementById("p1");
 var p2button = document.getElementById("p2");
-var h1 = document.querySelector("h1");
 var p1display = document.querySelector("#p1display");
-var p2display = document.querySelector("#p1display");
+var p2display = document.querySelector("#p2display");
 var p1score = 0;
 var p2score = 0;
-
+var gameOver = false;
+var winningScore = 5;
 
 p1button.addEventListener("click", function(){
-    p1score ++;
-    p1display.textContent = p1score;
+    if(!gameOver){
+        p1score ++;
+        if (p1score === winningScore) {
+            gameOver = true;
+        }
+        p1display.textContent = p1score;
+    }
 });
 
 p2button.addEventListener("click", function(){
-    p2score ++;
-    p2display.textContent = p2score;
+    if(!gameOver){
+        p2score ++;
+        if (p2score === winningScore) {
+            gameOver = true;
+        }
+        p2display.textContent = p2score;
+    }
 });
+
