f801f38eb04f6a8329ddc2124fdb7edbfe3ee944
Mayra Jaimes <mtj26@georgetown.edu>
2018-02-22 14:54:36 -0600

    added variables for score and to display information and event listeners to display new score

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,5 +1,18 @@
 var p1button = document.getElementById("p1");
+var p2button = document.getElementById("p2");
+var h1 = document.querySelector("h1");
+var p1display = document.querySelector("#p1display");
+var p2display = document.querySelector("#p1display");
+var p1score = 0;
+var p2score = 0;
+
 
 p1button.addEventListener("click", function(){
-    console.log("clicked");
+    p1score ++;
+    p1display.textContent = p1score;
+});
+
+p2button.addEventListener("click", function(){
+    p2score ++;
+    p2display.textContent = p2score;
 });
